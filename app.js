bd91937844b96ddbaf306024c9d5cdfad948b79e
Thays Silva <tplsilva05@gmail.com>
2024-02-09 17:58:42 -0300

    update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-constcaixaTexto = document.getElementById('texto-transcrito');
-constbotaoTranscricao = document.getElementById('transcrever');
-const reconhecimento = new webkitSpeeachRecognition();
+// Obtém uma referência ao elemento HTML com o id 'texto-transcrito'
+const caixaTexto = document.getElementById('texto-transcrito');
+// Obtém uma referência ao elemento HTML com o id 'transcrever'
+const botaoTranscricao = document.getElementById('transcrever');
+// Cria uma nova instância de webkitSpeechRecognition para reconhecimento de fala
+const reconhecimento = new webkitSpeechRecognition();
 
+// Define um manipulador de eventos para o evento 'result' do reconhecimento de fala
 reconhecimento.onresult = (evento) => {
-    const transcricao = evento.resultx [0][0]. trascript;
+    // Obtém a transcrição da fala a partir dos resultados do reconhecimento
+    const transcricao = evento.results[0][0].transcript;
+    // Define o texto da caixa de texto com a transcrição obtida
     caixaTexto.innerText = transcricao;
 };
 
+// Adiciona um ouvinte de evento ao botão de transcrição para iniciar o reconhecimento de fala
 botaoTranscricao.addEventListener('click', () => reconhecimento.start());
